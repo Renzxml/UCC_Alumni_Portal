f73bc3f1610786ee51eae6f91668e80a0be770f6
Renzxml <renzrodriguez23@gmail.com>
2025-09-10 12:08:25 +0800

    main

--- resources/views/user/community.blade.php
+++ resources/views/user/community.blade.php
@@ -4,6 +4,9 @@
 
 @section('content')
 <div>
+    <h3>Community Page</h3>
+    <p>Welcome to the community page!</p>
     <!-- He who is contented is rich. - Laozi -->
+     
 </div>
 @endsection
--- resources/views/user/network.blade.php
+++ resources/views/user/network.blade.php
@@ -4,6 +4,20 @@
 
 @section('content')
 <div>
-    <!-- He who is contented is rich. - Laozi -->
+    <h3>Network Page</h3>
+    <div class="network-container">
+        <p>Welcome to the network page!</p>
+        <div class="network-map">
+        </div>
+        <div class="network-details">
+            <input type="text" placeholder="Search network..." />
+            <ul>
+                <li>Caloocan</li>
+                <li>Manila</li>
+                <li>Quezon City</li>
+                <li>Pasig</li>
+            </ul>
+        </div>
+    </div>
 </div>
 @endsection
